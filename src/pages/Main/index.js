4bbfaccc4a5e81da0b3e7a858a00938434fd7b52
devCarlosALexandre <carloalexandresousa@gmail.com>
2022-12-16 10:14:16 -0300

    working styles

---
+++ src/pages/Main/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Form, SubmitButton } from './styles'
+import { FaGithub, FaPlus } from 'react-icons/fa'
+
+export default function Main() {
+    return (
+        <Container>
+            <h1>
+                <FaGithub size={25} />
+                Meus Repositórios
+            </h1>
+
+            <Form onSubmit={() => { }}>
+                <input type="text" placeholder="Adicionar Repositorios" />
+                <SubmitButton>
+                    <FaPlus color="#fff" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+`;
+export const Form = styled.div`
+
+`;
+export const SubmitButton = styled.div`
+
+`;
